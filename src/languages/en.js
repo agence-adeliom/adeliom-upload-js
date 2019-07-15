e3da58f5b78fa62d627ebba0b524311399295fbe
Antoine W <antoine-waag@hotmail.fr>
2019-07-15 23:52:54 +0200

    remove files

---
+++ src/languages/en.js
@@ -0,0 +1,16 @@
+export const en = {
+    textBtnUpload: 'Add a file',
+    textBeforeUpload: 'No file selected',
+    textAfterUpload: 'Your files',
+    textSizeRest: '{{number}} left',
+    textFileRest: '{{number}} file{{s}} left',
+    textMaxFileSize: '{{number}} maximum per file',
+    textFileNotValid: "Your file is not valid",
+    textMultipleFileNotValid: "Your file are not valid:",
+    textFileTooBig: "Your file is too big",
+    textMultipleFileTooBig: "Your files are too big",
+    textTotalFilesSize: "Total files is too big",
+    textTooManyFiles: "Too many files uploaded",
+    textTotalFiles: "The limit has been reached",
+    textErrorUpload: "Cannot upload the file"
+};
---
+++ src/languages/fr.js
@@ -0,0 +1,16 @@
+export const fr = {
+    textBtnUpload: 'Ajouter un fichier',
+    textBeforeUpload: 'Aucun fichier sélectionné',
+    textAfterUpload: 'Vos fichiers',
+    textSizeRest: '{{number}} restant{{s}}',
+    textFileRest: '{{number}} fichier{{s}} restant{{s}}',
+    textMaxFileSize: '{{number}} maximum par fichier',
+    textFileNotValid: "Votre fichier n'est pas valide:",
+    textMultipleFileNotValid: "Vos fichiers ne sont pas valides:",
+    textFileTooBig: "Votre fichier est trop lourd:",
+    textMultipleFileTooBig: "Vos fichiers sont trop lourd:",
+    textTotalFilesSize: "Le poids total de vos fichiers est trop lourd.",
+    textTooManyFiles: "Trop de fichiers téléchargés !",
+    textTotalFiles: "La limite du nombre de fichier est atteinte",
+    textErrorUpload: "Une erreur est survenue, impossible de télécharger le fichier."
+};
