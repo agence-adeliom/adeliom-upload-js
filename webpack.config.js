f0a6e187df352244227717c5911c56615d1d6e04
Antoine WAAG <antoine.waag@adeliom.com>
2019-09-04 14:57:52 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: "production",
-    entry: ["core-js/stable", "regenerator-runtime/runtime", "./src/upload"],
+    entry: "./src/upload",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "upload.js",
