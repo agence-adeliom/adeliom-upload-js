d55366732852cf2c877f5c7f4c39207ff9d48b42
Elo <emma.louviot@adeliom.com>
2020-03-05 10:00:49 +0100

    Ajout de fichier de trad : arabe, espagnol et russe

---
+++ src/languages/ar.js
@@ -0,0 +1,16 @@
+export const fr = {
+    textBtnUpload: "أضف ملف",
+    textBeforeUpload: "لم يتم اختيار اي ملف",
+    textAfterUpload: "الملفات الخاصة بك",
+    textSizeRest: "{{number}} المتبقي",
+    textFileRest: "{{number}} ملفات المتبقي",
+    textMaxFileSize: "{{number}} ملفات",
+    textFileNotValid: "الملف الخاص بك غير صالح",
+    textMultipleFileNotValid: "ملفاتك غير صالحة",
+    textFileTooBig: "الملف الذي حمّلته كبير جدًا",
+    textMultipleFileTooBig: "ملفاتك كبيرة جدًا",
+    textTotalFilesSize: "الوزن الكلي لملفاتك ثقيل للغاية",
+    textTooManyFiles: "تم تحميل الكثير من الملفات",
+    textTotalFiles: "تم الوصول إلى الحد الأقصى لعدد الملفات",
+    textErrorUpload: "حدث خطأ ، لا يمكن تحميل الملف"
+};
---
+++ src/languages/es.js
@@ -0,0 +1,16 @@
+export const en = {
+    textBtnUpload: 'Agregar un archivo',
+    textBeforeUpload: 'Ningún archivo seleccionado',
+    textAfterUpload: 'Sus archivos',
+    textSizeRest: '{{number}} restantes',
+    textFileRest: '{{number}} archivos restantes',
+    textMaxFileSize: '{{number}} archivos',
+    textFileNotValid: "Su archivo no es válido",
+    textMultipleFileNotValid: "Sus archivos no son válidos",
+    textFileTooBig: "Su archivo es demasiado grande",
+    textMultipleFileTooBig: "Sus archivos son demasiado grandes",
+    textTotalFilesSize: "El tamaño total de sus archivos es demasiado pesado",
+    textTooManyFiles: "Demasiados archivos cargados",
+    textTotalFiles: "Ha alcanzado el número máximo de archivos",
+    textErrorUpload: "Error, el archivo no puede ser cargado"
+};
